a1eb92836e72334cb8dff5d08f45f935f3dc7fc8
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-01 16:48:47 -0700

    Creating Tic tac toe UI

---
+++ src/components/Board.tsx
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+import {View} from 'react-native';
+import {Square} from './Square';
+import styles from '../../styles';
+
+export const Board = () => {
+  const [squares, setSquares] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+  const [playerTurn, setPlayerTurn] = useState(true);
+
+  const handleSquarePress = (x: number, y: number) => {
+    const value = playerTurn ? 'X' : 'O';
+    const newSquares = [...squares];
+
+    if (newSquares[x][y] !== '') {
+      return;
+    }
+
+    console.log(x, y, newSquares[x]);
+    newSquares[x][y] = value;
+
+    setPlayerTurn(!playerTurn);
+    console.log(newSquares);
+    setSquares(newSquares);
+  };
+
+  return (
+    <View style={styles.board} testID="board">
+      {squares.map((row, rowIndex) => (
+        <View key={`row-${rowIndex}`} style={styles.row}>
+          {row.map((square: string, index: number) => (
+            <Square
+              y={index}
+              x={rowIndex}
+              value={square}
+              key={`square-${rowIndex}${index}`}
+              handleOnPress={handleSquarePress}
+            />
+          ))}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default Board;
